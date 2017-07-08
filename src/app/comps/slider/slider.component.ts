6c67ee0c0b0779a6b183790e11dd3bfe669d5cc3
Daniel Kahle <daniel.kahle@acxiom.com>
2017-07-08 10:25:55 -0700

    slider

--- src/app/comps/slider/slider.component.ts
+++ src/app/comps/slider/slider.component.ts
@@ -5,18 +5,27 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss']
 })
-export class SliderComponent implements OnInit {
+export class SliderComponent {
 
   min = 0;
   max = 12;
   step = 1 // what granularity they can pick, what it will snap to
-  tickInterval = 6; // where the ticks show up
+  ticks = true;
+  auto = false;
+  _tickInterval:any = 6; // where the ticks show up
   thumbLabel = true;
   log = console.log;
 
-  constructor() { }
-
-  ngOnInit() {
+  get tickInterval() {
+    if (!this.ticks) {
+      return undefined;
+    }
+    else {
+      return this.auto? 'auto': this._tickInterval;
+    }
   }
 
+  set tickInterval(val:number) {
+    this._tickInterval = val;
+  }
 }
